b3e09996461322d67d439dd8e6e62e74dd0e23b1
Othmaneb9 <othmane.baar@gmail.com>
2024-06-12 18:59:26 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,29 +1,28 @@
-import './App.css';
 import { useState } from "react"
 
 function LoginPage() {
+// state
 const [prenom, setPrenom] = useState("") 
 
-
-const handleClick = () => {
-  alert(`Bonjour ` + prenom)
-}
-
+// comportements
 const handleSubmit = (event) => {
   event.preventDefault();
+  alert(`Bonjour ` + prenom)
+  setPrenom("")
 }
 
 const handleChange = (event) => {
   setPrenom(event.target.value);
 }
 
+// Affichage
   return (
     <div className="LoginPage">
-    <h1>Bienvenue chez nous !</h1>
+    <h1>Bienvenue chez nous !</h1><br/>
     <h2>Connectez vous</h2>
     <form action='submit' onSubmit={handleSubmit}>
       <input value = {prenom} type='text' id='name' required='required' placeholder='Entrez votre prénom' onChange={handleChange}></input>
-      <button onClick={handleClick} >Accédez à votre Espace</button>
+      <button>Accédez à votre Espace</button>
     </form>
     </div>
   );
